b5c8c1256fac0aabe5f09fdbd7d381752a3ee30c
Volvapp <chriszp2905@gmail.com>
2025-06-24 23:57:02 +0300

    init commit

---
+++ src/main/java/com/uni/GradeCenter/GradeCenterApplication.java
@@ -0,0 +1,13 @@
+package com.uni.GradeCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeCenterApplication.class, args);
+	}
+
+}
